c11862b52850b0e39217871ba329f36b2316a977
Karla Herzo ⚡+ <127571238+kherzo99@users.noreply.github.com>
2023-11-11 16:40:59 -0600

    testing post creation on database

---
+++ js/createPost.js
@@ -0,0 +1,48 @@
+alert("Js conectado");
+
+const createPost = () => {
+  let postImg = document.getElementById("post-image-input").value;
+  let title = document.getElementById("FormControlTextarea1").value;
+  let postContent = document.getElementById("FormControlTextarea2").value;
+  let timeToRead = document.getElementById("FormControlTextarea3").value;
+  let hashtag1 = document.getElementById("FormControlHashtags").value;
+  let userName = document.getElementById("post-author-input").value;
+  let userImg = document.getElementById("post-author-picture").value;
+
+  let date = new Date().toLocaleDateString();
+
+  let postObject = {
+    postImg,
+    title,
+    postContent,
+    timeToRead,
+    hashtag1,
+    date,
+    userName,
+    userImg,
+  };
+  return postObject;
+};
+
+const createPostinDB = async (postObject) => {
+  let response = await fetch(
+    "https://devtodatabase-950dc-default-rtdb.firebaseio.com/posts/post1.json",
+    {
+      method: "POST",
+      body: JSON.stringify(postObject),
+    }
+  );
+  let data = await response.json();
+  return data;
+};
+
+const savePost = async () => {
+  let postObject = createPost();
+  console.log(postObject);
+  let response = await createPostinDB(postObject);
+  console.log(response);
+};
+
+let savePostBtn = document.getElementById("publish-btn");
+
+savePostBtn.addEventListener("click", savePost);
